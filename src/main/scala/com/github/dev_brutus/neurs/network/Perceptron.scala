f0c5db73bd9bca98975e128478349db16ec044fa
Brutus <dev.brutus@gmail.com>
2016-04-27 23:53:01 +0300

    apply

--- src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
+++ src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
@@ -11,6 +11,8 @@ class Perceptron(val layers: Seq[Int]) {
     (0 until layer._1).map(l => Symbol(s"${layer._2}_$l"))
   }.toList
 
+  val lastLayerNeurons = neuronsPerLayer.last
+
   val neuronsByOrder = neuronsPerLayer.flatten
 
   val weights: Map[(Symbol, Symbol), Double] = {
@@ -23,8 +25,13 @@ class Perceptron(val layers: Seq[Int]) {
 
   val toFrom = weights.keys.groupBy(_._2)
 
-  def apply(initValues:Map[Symbol, Double]) = {
+  def apply(initValues: Map[Symbol, Double]) ={
+    val networkValues = neuronsByOrder.foldLeft(initValues) { (acc, neuron) =>
+      val neuronValue : Double = toFrom(neuron).view.map(synapse => weights(synapse) * acc(synapse._1)).sum
+      acc + (neuron -> neuronValue)
+    }
 
+    lastLayerNeurons.view.map(neuron => neuron -> networkValues(neuron)).toMap
   }
 
   override def toString = s"Perceptron(weights=$weights)"
