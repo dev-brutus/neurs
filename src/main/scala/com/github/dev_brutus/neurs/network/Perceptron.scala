25c2e154da51d26fd55fd34d01ed164a39f9b685
Brutus <dev.brutus@gmail.com>
2016-04-28 00:07:51 +0300

    copy

--- src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
+++ src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
@@ -5,34 +5,41 @@ import scala.collection.immutable.IndexedSeq
 /**
   *
   */
-class Perceptron(val layers: Seq[Int]) {
+class Perceptron(val neuronsPerLayer: List[IndexedSeq[Symbol]], injectedWeights: Map[(Symbol, Symbol), Double] = Map.empty) {
 
-  val neuronsPerLayer = layers.view.zipWithIndex.map { layer =>
-    (0 until layer._1).map(l => Symbol(s"${layer._2}_$l"))
-  }.toList
-
-  val lastLayerNeurons = neuronsPerLayer.last
-
-  val neuronsByOrder = neuronsPerLayer.flatten
+  def this(layers: Iterable[Int]) {
+    this(layers.view.zipWithIndex.map { layer =>
+      (0 until layer._1).map(l => Symbol(s"${layer._2}_$l"))
+    }.toList)
+  }
 
   val weights: Map[(Symbol, Symbol), Double] = {
     neuronsPerLayer.sliding(2).flatMap { pair =>
       val l0: IndexedSeq[Symbol] = pair(0)
       val l1: IndexedSeq[Symbol] = pair(1)
       l0.flatMap(l => l1.map((l, _)))
-    }.map((_, 0.5)).toMap
+    }.map { synapse =>
+      synapse -> injectedWeights.getOrElse(synapse, 0.5)
+    }.toMap
   }
+
+  val lastLayerNeurons = neuronsPerLayer.last
+
+  val neuronsByOrder = neuronsPerLayer.flatten
 
   val toFrom = weights.keys.groupBy(_._2)
 
-  def apply(initValues: Map[Symbol, Double]) ={
+  def apply(initValues: Map[Symbol, Double]) = {
     val networkValues = neuronsByOrder.foldLeft(initValues) { (acc, neuron) =>
-      val neuronValue : Double = toFrom(neuron).view.map(synapse => weights(synapse) * acc(synapse._1)).sum
+      val neuronValue: Double = toFrom(neuron).view.map(synapse => weights(synapse) * acc(synapse._1)).sum
       acc + (neuron -> neuronValue)
     }
 
     lastLayerNeurons.view.map(neuron => neuron -> networkValues(neuron)).toMap
   }
+
+  def changeWeights(newWeights: Map[(Symbol, Symbol), Double]) =
+    new Perceptron(neuronsPerLayer, weights ++ newWeights)
 
   override def toString = s"Perceptron(weights=$weights)"
 }
