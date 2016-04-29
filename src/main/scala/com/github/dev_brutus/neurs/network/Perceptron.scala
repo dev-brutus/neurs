7f657891e627c5b5b6eed44def98aa0e1165b746
Brutus <dev.brutus@gmail.com>
2016-04-29 15:15:19 +0300

    refactor

--- src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
+++ src/main/scala/com/github/dev_brutus/neurs/network/Perceptron.scala
@@ -5,32 +5,12 @@ import scala.collection.immutable.IndexedSeq
 /**
   *
   */
-class Perceptron(val neuronsPerLayer: List[IndexedSeq[Symbol]], injectedWeights: Map[(Symbol, Symbol), Double] = Map.empty) {
-
-  def this(layers: Iterable[Int]) {
-    this(layers.view.zipWithIndex.map { layer =>
-      (0 until layer._1).map(l => Symbol(s"${Base26(layer._2)}$l"))
-    }.toList)
-  }
-
-  val weights: Map[(Symbol, Symbol), Double] = {
-    neuronsPerLayer.sliding(2).flatMap { pair =>
-      val l0: IndexedSeq[Symbol] = pair(0)
-      val l1: IndexedSeq[Symbol] = pair(1)
-      l0.flatMap(l => l1.map((l, _)))
-    }.map { synapse =>
-      synapse -> injectedWeights.getOrElse(synapse, 0.5)
-    }.toMap
-  }
-
-  val lastLayerNeurons = neuronsPerLayer.last
-
-  val neuronsByOrder = neuronsPerLayer.flatten
+class Perceptron(val neurons: Iterable[Symbol], val weights: Map[(Symbol, Symbol), Double], val outputNeurons: Iterable[Symbol]) {
 
   val toFrom = weights.keys.groupBy(_._2)
 
   def apply(initValues: Map[Symbol, Double]) = {
-    val networkValues = neuronsByOrder.foldLeft(initValues) { (acc, neuron) =>
+    val networkValues = neurons.foldLeft(initValues) { (acc, neuron) =>
       acc.contains(neuron) match {
         case true => acc
         case false =>
@@ -39,11 +19,40 @@ class Perceptron(val neuronsPerLayer: List[IndexedSeq[Symbol]], injectedWeights:
       }
     }
 
-    lastLayerNeurons.view.map(neuron => neuron -> networkValues(neuron)).toMap
+    outputNeurons.view.map(neuron => neuron -> networkValues(neuron)).toMap
   }
 
   def changeWeights(newWeights: Map[(Symbol, Symbol), Double]) =
-    new Perceptron(neuronsPerLayer, weights ++ newWeights)
+    new Perceptron(neurons, weights ++ newWeights, outputNeurons)
 
   override def toString = s"Perceptron(weights=$weights)"
+}
+
+object Perceptron {
+
+  def apply(layers: Iterable[Int]) = {
+    val neuronsByLayer = layers.view.zipWithIndex.map { layer =>
+      (0 until layer._1).map(l => Symbol(s"${Base26(layer._2)}$l"))
+    }.toList
+    apply(neuronsByLayer, Map.empty())
+  }
+
+  def apply(neuronsPerLayer: Seq[IndexedSeq[Symbol]], injectedWeights: Map[(Symbol, Symbol), Double) = {
+
+    val weights: Map[(Symbol, Symbol), Double] = {
+      neuronsPerLayer.sliding(2).flatMap { pair =>
+        val l0: IndexedSeq[Symbol] = pair(0)
+        val l1: IndexedSeq[Symbol] = pair(1)
+        l0.flatMap(l => l1.map((l, _)))
+      }.map { synapse =>
+        synapse -> injectedWeights.getOrElse(synapse, 0.5)
+      }.toMap
+    }
+
+    val lastLayerNeurons: Seq[Symbol] = neuronsPerLayer.last
+
+    val neuronsByOrder: Iterable[Symbol] = neuronsPerLayer.flatten
+
+    new Perceptron(neuronsByOrder, weights, lastLayerNeurons)
+  }
 }
